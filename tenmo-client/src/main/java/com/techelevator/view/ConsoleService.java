6bec5e4f148c1d18a3bec0fd59dc75f1786827f8
Lisa King <lisa.king05@outlook.com>
2021-02-06 13:49:53 -0500

    Added print statements to ConsoleService

--- tenmo-client/src/main/java/com/techelevator/view/ConsoleService.java
+++ tenmo-client/src/main/java/com/techelevator/view/ConsoleService.java
@@ -6,7 +6,9 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+import com.techelevator.tenmo.models.AccountBalance;
 import com.techelevator.tenmo.models.Transfer;
+import com.techelevator.tenmo.services.TransferService;
 
 public class ConsoleService {
 
@@ -76,11 +78,46 @@ public class ConsoleService {
 		return result;
 	}
 	
+	//Use Case 3
+	public void printCurrentAccountBalance(AccountBalance accountBalance) {
+		out.println("Your current account balance is: " + accountBalance.getBalance());
+	}
 	
+	//Use Case 4
+	public void printSendTEBucks(Transfer transfer) {
+		out.println("-------------------------------------------");
+		out.println(transfer.getTransferId() + " Name");
+		out.println("-------------------------------------------");
+		//while loop
+		out.println("---------");
+		out.println("Enter " + transfer.getTransferId() + " of" + transfer.getTransferTo() + " you are sending to (0 to cancel) ");
+		out.println("Enter " + transfer.getTransferAmount());
+	}
+	
+	//Use Case 5
+	public void printViewTransfers(Transfer transfer) {
+		out.println("-------------------------------------------");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	//Use Case 6
 	public void printTransferDetails(Transfer transfer) {
-		out.println("-------------");
+		out.println("--------------------------------------------");
+		out.println("Transfer Details");
+		out.println("--------------------------------------------");
 		out.println(transfer.getTransferId());
 		out.println(transfer.getTransferFrom());
+		out.println(transfer.getTransferTo());
+		out.println(transfer.getTransferType());
+		out.println(transfer.getTransferStatus());
+		out.println(transfer.getTransferAmount());
 	}
 	
 	//TODO printTransfer method that loops through transfers and prints them one by one-- returning a transfer array
